83bc7e29362ac9723f6f4d764b778efffc065e04
Axel <axosar@gmail.com>
2019-10-08 12:12:11 -0500

    Adding link to logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from './components/Home';
 import Introduccion from './components/Introduccion';
 import './App.css';
@@ -9,7 +9,9 @@ function App() {
     <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <header className="App-header">
-          <img src="http://www.imcyc.com/wp-content/uploads/2017/07/logo_200.png" alt="" title="" />
+          <Link to="/introduccion">
+            <img src="http://www.imcyc.com/wp-content/uploads/2017/07/logo_200.png" alt="" title="" />
+          </Link>
           <Route exact path="/" component={Home} />
           <Route exact path="/introduccion" component={Introduccion} />
         </header>
