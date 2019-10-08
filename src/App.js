ed3d7c4bd029ac00419a60544cd722e7e04ce73d
Axel <axosar@gmail.com>
2019-10-08 11:18:18 -0500

    Agregando el titulo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src="http://www.imcyc.com/wp-content/uploads/2017/07/logo_200.png" alt="" title="" />
         <h1>MANUAL DE AGREGADOS PARA CONCRETO</h1>
         <p>Instituto Mexicano del Cemento y del Concreto A.C.</p>
       </header>
