33219db36c35857e9f67b917539548652c03071e
Axel <axosar@gmail.com>
2019-10-08 11:50:39 -0500

    Adding router for git

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Route exact path="/" component={Home} />
         <Route exact path="/introduccion" component={Introduccion} />
